a68fbeeea2be3886dd3d0f669bf77b99d465cdb9
samchat2561 saetieaw <samchat2561@gmail.com>
2026-01-05 20:32:47 +0700

    update

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,6 +5,7 @@ namespace App\Filament\Widgets;
 use App\Models\Post;
 use App\Models\User;
 use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\Cache;
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Cache;
 class StatsOverview extends StatsOverviewWidget
 {
     use HasWidgetShield;
+    protected ?string $pollingInterval = '10s';
     protected function getStats(): array
     {
         $stats = Cache::remember('dashboard_stats', now()->addMinute(5), function () {
@@ -29,9 +31,31 @@ class StatsOverview extends StatsOverviewWidget
             ];
         });
         return [
-            Stat::make('Total Post', $stats['totalPosts']),
-            Stat::make('Total Pages', $stats['totalPages']),
+            Stat::make('Total Post', $stats['totalPosts'])
+                ->description('Total number of post')
+                ->descriptionIcon('heroicon-o-document-text', IconPosition::Before)
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Total Pages', $stats['totalPages'])
+                ->description('Total number of pages')
+                ->descriptionIcon('heroicon-o-document-check', IconPosition::Before)
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('warning'),
             Stat::make('Total Users', $stats['totalUsers'])
+                ->description('Total number of users')
+                ->descriptionIcon('heroicon-o-user-group', IconPosition::Before)
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('primary'),
         ];
+    }
+
+    protected function getHeading(): ?string
+    {
+        return 'Analytics';
+    }
+
+    protected function getDescription(): ?string
+    {
+        return 'An overview of some analytics.';
     }
 }
